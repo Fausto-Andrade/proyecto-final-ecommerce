ec9377af5455fe460fdceef0a0f367b61b55bc01
Fausto Andrade <faustoantonioandradevera@gmail.com>
2024-04-25 22:53:31 -0500

    Create Cart

--- src/tests/product.test.js
+++ src/tests/product.test.js
@@ -1,3 +1,4 @@
+require("../models")
 const Category = require("../models/Category");
 const request = require("supertest")
 const app = require('../app')
@@ -5,6 +6,8 @@ const URL_BASE = '/api/v1/products';
 
 let category
 let TOKEN
+let productId
+let product
 
 beforeAll(async () => {
 
@@ -24,7 +27,7 @@ beforeAll(async () => {
 
 test("POST 'URL_BASE', should return statusCode 201, and res.body.title === products.title", async () => {
 
-    const product = {
+      product = {
       title: "Celular",
       description: "iphone 15 256gb",
       price: 890,
@@ -35,11 +38,62 @@ test("POST 'URL_BASE', should return statusCode 201, and res.body.title === prod
       .post(URL_BASE)
       .send(product)
       .set("Authorization", `Bearer ${TOKEN}`)
+
+      productId = res.body.id
   
     expect(res.status).toBe(201)
     expect(res.body).toBeDefined()
     expect(res.body.title).toBe(product.title)
 
+  })
+
+  test("GET 'URL_BASE', should return statusCode 200, and res.body.length === 1", async () => {
+    
+    const res = await request(app)
+        .get(URL_BASE)
+  
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
+
+    
+})
+
+test("GET -> URL_BASE/:id, should return statusCode 201, and res.body.length ===1 ", async () => {
+    const res = await request(app)
+        .get(`${URL_BASE}/${productId}`)
+    
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.title).toBe(product.title)
+
+    // Siempre va en la ultima linea del ultimo test a realizar.
+}) 
+
+test("PUT -> URL_BASE, should return statusCode 200, and res.body.title === bodyUpdate.title", async () => {
+
+    const bodyUpdate = {
+        title: "iphone 15 pro max"
+    }
+    
+    const res = await request(app)
+        .put(`${URL_BASE}/${productId}`)
+        .send(bodyUpdate)
+        .set("Authorization", `Bearer ${TOKEN}`)
+    
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.title).toBe(bodyUpdate.title)
+})
+
+
+test('Delete -> BASE_URL, should return statusCode 204', async () => {
+    const res = await request(app)
+      .delete(`${URL_BASE}/${productId}`)
+      .set('Authorization', `Bearer ${TOKEN}`)
+    
+    expect(res.statusCode).toBe(204)
+    
     await category.destroy()
 
-  })
+    })
